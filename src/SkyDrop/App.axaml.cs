e1c4aa8c08999ee8a70d7b71a38ced9bc469f02d
Timothy Miller <innerlogic4321@gmail.com>
2026-01-08 15:14:30 +0900

    Start apps

---
+++ src/SkyDrop/App.axaml.cs
@@ -0,0 +1,35 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Data.Core;
+using Avalonia.Data.Core.Plugins;
+using System.Linq;
+using Avalonia.Markup.Xaml;
+using SkyDrop.Views;
+
+namespace SkyDrop;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            desktop.MainWindow = new MainWindow
+            {
+            };
+        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        {
+            singleViewPlatform.MainView = new MainView
+            {
+            };
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
